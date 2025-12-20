2c9954eaa8c1f22f2fa0678d225e780544cc7b1f
Heisenkebab <winnoa21@htl-kaindorf.at>
2025-12-20 11:00:24 +0100

    feat(audio): Add audio config

--- nixos/hardware/default.nix
+++ nixos/hardware/default.nix
@@ -1,6 +1,7 @@
 _: {
   imports = [
     ./network
+    ./media
     ./amdgpu.nix
     ./disko.nix
   ];
---
+++ nixos/hardware/media/pipewire.nix
@@ -0,0 +1,25 @@
+_: {
+  services.pulseaudio.enable = false;
+
+  # use the realtime scheduler for increased performance
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
+
+    alsa = {
+      enable = true;
+      support32Bit = false;
+    };
+
+    extraConfig.pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 512;
+        "default.clock.min-quantum" = 128;
+        "default.clock.max-quantum" = 1024;
+      };
+    };
+  };
+}
