d18d4e0f3d3125cf02424d929904d06e4bff400c
Heisenkebab <winnoa21@htl-kaindorf.at>
2025-12-20 11:22:43 +0100

    feat(modules): Add nix-ld

--- modules/security.nix
+++ modules/security.nix
@@ -1,4 +1,11 @@
 {pkgs, ...}: {
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    openssl
+    glibc
+  ];
+
   environment.systemPackages = with pkgs; [
     # tools
     git-cliff
