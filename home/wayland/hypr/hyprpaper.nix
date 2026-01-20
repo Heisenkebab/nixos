9953d83d938785be78b3c74514ac8b1137c46357
Heisenkebab <winnoa21@htl-kaindorf.at>
2026-01-20 12:59:57 +0100

    feat(laptop,hyprpaper): Add new hyprpaper and add host specific wallpapers

--- home/wayland/hypr/hyprpaper.nix
+++ home/wayland/hypr/hyprpaper.nix
@@ -1,5 +1,23 @@
-_: {
+{meta, ...}:
+if meta.name == "laptop"
+then {
   home.file.".config/hypr/hyprpaper.conf".text = ''
-    preload = ${../../../wallpapers/wallhaven-blonde.png}
-    wallpaper = , ${../../../wallpapers/wallhaven-blonde.png} '';
+    preload = ${../../../wallpapers/wallhaven-black.png}
+    preload = ${../../../wallpapers/wallhaven-forest.jpg}
+    wallpaper = eDP-1, ${../../../wallpapers/wallhaven-black.png}
+    wallpaper = HDMI-A-1, ${../../../wallpapers/wallhaven-forest.jpg}
+  '';
+}
+else if meta.name == "pc"
+then {
+  home.file.".config/hypr/hyprpaper.conf".text = ''
+    preload = ${../../../wallpapers/wallhaven-black.png}
+    wallpaper = , ${../../../wallpapers/wallhaven-black.png}
+  '';
+}
+else {
+  home.file.".config/hypr/hyprpaper.conf".text = ''
+    preload = ${../../../wallpapers/wallhaven-black.png}
+    wallpaper = , ${../../../wallpapers/wallhaven-black.png}
+  '';
 }
