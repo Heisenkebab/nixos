885af0b3582a06dc63d74ef6765bfa17490ae028
Heisenkebab <winnoa21@htl-kaindorf.at>
2026-04-03 12:58:01 +0200

    refactor(git): replace deprecated code

---
+++ home/shared/dev/git.nix
@@ -0,0 +1,23 @@
+_: {
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Heisenkebab";
+        email = "winnoa21@htl-kaindorf.at";
+      };
+
+      aliases = {
+        open = "!sh -c 'url=$(git config --get remote.origin.url); if [[ $url == git@* ]]; then url=$(echo $url | sed -e \"s/:/\\//\" -e \"s/^git@/https:\\/\\//\" -e \"s/\\.git$//\"); fi; xdg-open $url'";
+      };
+
+      pull = {
+        rebase = true;
+      };
+
+      credential.helper = "!gh auth git-credential";
+    };
+
+    ignores = ["*~" ".DS_Store" ".direnv"];
+  };
+}
