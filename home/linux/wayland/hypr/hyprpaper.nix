1ea8177b110a6d5ea6f83790781ee108157e43bf
Heisenkebab <winnoa21@htl-kaindorf.at>
2026-08-12 17:10:55 +0200

    fix(hyprpaper): fix relative path from hyprpaper

--- home/linux/wayland/hypr/hyprpaper.nix
+++ home/linux/wayland/hypr/hyprpaper.nix
@@ -2,22 +2,22 @@
 if meta.name == "laptop"
 then {
   home.file.".config/hypr/hyprpaper.conf".text = ''
-    preload = ${../../../wallpapers/wallhaven-black.png}
-    preload = ${../../../wallpapers/wallhaven-forest.jpg}
-    wallpaper = eDP-1, ${../../../wallpapers/wallhaven-black.png}
-    wallpaper = HDMI-A-1, ${../../../wallpapers/wallhaven-forest.jpg}
+  preload = ${../../../../wallpapers/wallhaven-black.png}
+  preload = ${../../../../wallpapers/wallhaven-forest.jpg}
+  wallpaper = eDP-1, ${../../../../wallpapers/wallhaven-black.png}
+  wallpaper = HDMI-A-1, ${../../../../wallpapers/wallhaven-forest.jpg}
   '';
 }
 else if meta.name == "pc"
 then {
   home.file.".config/hypr/hyprpaper.conf".text = ''
-    preload = ${../../../wallpapers/wallhaven-black.png}
-    wallpaper = , ${../../../wallpapers/wallhaven-black.png}
+  preload = ${../../../../wallpapers/wallhaven-black.png}
+  wallpaper = , ${../../../../wallpapers/wallhaven-black.png}
   '';
 }
 else {
   home.file.".config/hypr/hyprpaper.conf".text = ''
-    preload = ${../../../wallpapers/wallhaven-black.png}
-    wallpaper = , ${../../../wallpapers/wallhaven-black.png}
+  preload = ${../../../../wallpapers/wallhaven-black.png}
+  wallpaper = , ${../../../../wallpapers/wallhaven-black.png}
   '';
 }
