3dfbf8f0f936984ada1a4faee50cc5041d39172d
Heisenkebab <heisenkebab@proton.me>
2026-09-25 20:50:23 +0200

    chore: remove unnecessary zsh.nix in darwin

--- home/darwin/default.nix
+++ home/darwin/default.nix
@@ -1,6 +1,5 @@
 _: {
   imports = [
     ./sketchybar
-    ./dev
   ];
 }
--- home/darwin/dev/shells/zsh.nix
+++
@@ -1,5 +0,0 @@
-{lib, ...}: {
-  programs.zsh.initContent = lib.mkAfter ''
-    export DOCKER_HOST="unix://$HOME/.lima/default/sock/docker.sock"
-  '';
-}
